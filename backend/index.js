bc4ecf9a6fc877b4afd06c65c079988aebfa9f15
Wickramasinghe W A N P <139307869+nethmina1028@users.noreply.github.com>
2024-06-19 14:09:48 +0530

    edit note

--- backend/index.js
+++ backend/index.js
@@ -169,6 +169,51 @@ app.use(
        }
        
    });
+                    
+                  //Edit Notes
+  
+    app.put("/edit-note/:noteId",authenticateToken, async (req,res)=>{
+        
+         const noteId = req.params.noteId;
+         const {title,content,tags,isPinned} = req.body;
+         const {user} = req.user;
+
+          if(!title && !content && !tags){
+              return res
+              .status(400)
+              .json({error:true,message:"No changes provided"});
+
+          }
+          try{
+            const note = await Note.findOne({_id: noteId, userId:user._id});
+
+            if(!note){
+                return res.status(404).json({error:true,message:"Note not found"});
+            
+            }
+
+            if (title) note.title = title;
+            if (content) note.content = content;
+            if (tags) note.tags = tags;
+            if (isPinned) note.isPinned = isPinned;
+
+            await note.save();
+
+            return res.json({
+              error:false,
+              note,
+              message:"Note updated successfully",
+            });
+          } catch(error){
+
+            return res.status(500).json({
+              error:true,
+              message:"Internal server error",
+            });
+
+          }
+                  
+    })
 
   
   
