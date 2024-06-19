a49283dcf6c4c4da3c8ec603bb7b7b5a96552e77
Wickramasinghe W A N P <139307869+nethmina1028@users.noreply.github.com>
2024-06-19 09:23:59 +0530

    Add notes api

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ app.use(
 
   }); 
 
-    //create account 
+                     //create account 
     app.post("/create-account", async (req,res)=>{
 
          const {fullName,email,password} = req.body;
@@ -83,7 +83,7 @@ app.use(
     });
 
 
-    //login
+                    //login
 
     app.post("/login", async (req,res)=>{
       
@@ -128,7 +128,50 @@ app.use(
           
     });
 
+                   //Add Notes
 
+    app.post("/add-note",authenticateToken, async (req,res)=>{
+           
+      const {title,content,tags} = req.body;
+      const {user} = req.user;
+
+       if(!title){
+           return res.status(400).json({error:true,message:"Title is required"});
+       }
+
+       if(!content){
+           return res.status(400).json({error:true,message:"Content is required"});
+       }
+
+       try{
+         const note = new Note({
+           title,
+           content,
+           tags: tags || [],
+           userId: user._id,
+
+         });
+
+         await note.save();
+
+         return res.json({
+           error:false,
+           note,
+           message:"Note added successfully",
+           
+         });
+
+       }catch(error){
+           return res.status(500).json({
+               error:true,
+               message:"Internal server error",
+           });
+       }
+       
+   });
+
+  
+  
 
     
 
--- backend/utilities.js
+++ backend/utilities.js
@@ -5,7 +5,7 @@ function authenticateToken(req,res,next){
     const authHeader =req.headers["authorization"];
     const token = authHeader && authHeader.split(' ')[1];
 
-    if(!token) return res.status(401);
+    if(!token) return res.sendStatus(401);
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if(err) return res.sendStatus(401);
@@ -15,4 +15,4 @@ function authenticateToken(req,res,next){
 
 }
 
-module.exports = authenticateToken;
+module.exports = {authenticateToken};
